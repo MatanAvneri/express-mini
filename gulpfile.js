26e9097d2f4d04c13bb27adbae002c3ba6fa2bd3
matanavneri <matan.av@gmail.com>
2017-04-13 14:50:33 +0300

    added gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,8 @@
-/**
- * Created by matanavneri on 4/13/17.
- */
+var gulp = require('gulp'),
+  template = require('gulp-template')
+
+gulp.task('default', function () {
+  return gulp.src(__dirname + '/app/templates/**')
+    .pipe(template({})) // Empty data for now
+    .pipe(gulp.dest('./')); // Relative to cwd
+})
